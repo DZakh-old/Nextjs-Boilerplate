749f5d3e68f031d37779fcd04ec9ad915fabf831
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-06-12 20:24:22 +0300

    Add custom app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { AppProps } from 'next/app';
+
 import '../styles/main.scss';
 
-const App = ({ Component, pageProps }: any) => {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
