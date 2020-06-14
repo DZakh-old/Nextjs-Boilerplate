c22c692595613f90d1dc0332bb9846e7e2d4c8cd
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-06-14 14:56:23 +0300

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,76 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable import/no-extraneous-dependencies */
-const withPlugins = require('next-compose-plugins');
-const withFonts = require('next-fonts');
-const withOptimizedImages = require('next-optimized-images');
-const withSass = require('@zeit/next-sass');
 
-module.exports = withPlugins([[withSass, { cssModules: true }], withFonts, withOptimizedImages]);
+const path = require('path');
+
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+const withPlugins = require('next-compose-plugins');
+const withSass = require('@zeit/next-sass');
+const withOptimizedImages = require('next-optimized-images');
+
+const sassConfig = {
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[local]___[name]___[hash:base64:5]',
+  },
+};
+
+const optimizedImagesConfig = {
+  inlineImageLimit: 8192,
+  imagesFolder: 'images',
+  imagesName: '[name]-[hash].[ext]',
+  optimizeImagesInDev: false,
+  mozjpeg: {
+    quality: 80,
+  },
+  optipng: {
+    optimizationLevel: 3,
+  },
+  pngquant: false,
+  gifsicle: {
+    interlaced: true,
+    optimizationLevel: 3,
+  },
+  svgo: {
+    // enable/disable svgo plugins here
+  },
+  webp: {
+    preset: 'default',
+    quality: 75,
+  },
+};
+
+const nextConfiguration = {
+  publicRuntimeConfig: {
+    AMO_API_URL: process.env.AMO_API_URL,
+  },
+  webpack: (config, options) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+
+    config.plugins.push(
+      new FilterWarningsPlugin({
+        exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
+      })
+    );
+
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|otf|ttf)(\?v=\d\.\d\.\d)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 100000,
+        esModule: false,
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [
+    [withSass, sassConfig],
+    [withOptimizedImages, optimizedImagesConfig],
+  ],
+  nextConfiguration
+);
--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -12,6 +12,14 @@ declare module '*.svg' {
   const value: any;
   export = value;
 }
+declare module '*.svg?sprite' {
+  const value: any;
+  export = value;
+}
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
 declare module '*.pdf' {
   const value: any;
   export = value;
