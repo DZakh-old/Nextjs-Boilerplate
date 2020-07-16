462414192507bd943ed76e179664e2619312d55c
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-07-17 01:03:28 +0300

    Add StoreProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from 'next/app';
 
+import { useInitialStore, StoreProvider } from '@/models';
+
 import '@/styles/main.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const $rootStore = useInitialStore(pageProps.initialState);
+
+  return (
+    <StoreProvider store={$rootStore}>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 };
 
 export default App;
