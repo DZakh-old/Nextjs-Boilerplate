fcec4800a50e499967f48181e86269c41fd33bd2
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-10-14 23:02:06 +0300

    Fix body-scroll

--- src/utils/body-scroll.ts
+++ src/utils/body-scroll.ts
@@ -6,19 +6,20 @@ export const getScrollbarWidth: () => number = () => {
 
 // Запоминаем ширину скрола для функции allowScroll
 let scrollbarWidth = 0;
+let isPrevented = false;
 let overflowPropBefore: string | null = null;
 
 /**
- * @description Если элемент владеет свойством rigth, меняем его на коэффициент
+ * @description Если элемент владеет свойством right, меняем его на коэффициент
  * @param  {element} el
  * @param  {number} coefficient
  */
 const adjustRightProp: (el: HTMLElement, coefficient: number) => void = (el, coefficient) => {
-  const rightProp = Number.parseFloat(el.style.right);
-
-  if (!rightProp) {
+  if (!el) {
     return;
   }
+
+  const rightProp = Number.parseFloat(el.style.right) || 0;
 
   el.style.setProperty('right', `${rightProp + coefficient}px`);
 };
@@ -29,17 +30,22 @@ const adjustRightProp: (el: HTMLElement, coefficient: number) => void = (el, coe
  * @param  {element[]} fixedEls Массив элементов с position: fixed
  */
 export const preventScroll: (fixedEls?: HTMLElement[]) => void = (fixedEls) => {
+  if (isPrevented) {
+    return;
+  }
+
   scrollbarWidth = getScrollbarWidth();
 
   overflowPropBefore = document.body.style.getPropertyValue('overflow');
   document.body.style.setProperty('overflow', 'hidden');
+
+  isPrevented = true;
 
   if (!scrollbarWidth) {
     return;
   }
 
   document.documentElement.style.setProperty('margin-right', `${scrollbarWidth}px`);
-
   forEach(fixedEls, (el) => {
     adjustRightProp(el, scrollbarWidth);
   });
@@ -51,12 +57,18 @@ export const preventScroll: (fixedEls?: HTMLElement[]) => void = (fixedEls) => {
  * @param  {element[]} fixedEls Массив элементов с position: fixed
  */
 export const allowScroll: (fixedEls?: HTMLElement[]) => void = (fixedEls) => {
+  if (!isPrevented) {
+    return;
+  }
+
   if (overflowPropBefore) {
     document.body.style.setProperty('overflow', overflowPropBefore);
     overflowPropBefore = null;
   } else {
     document.body.style.removeProperty('overflow');
   }
+
+  isPrevented = false;
 
   if (!scrollbarWidth) {
     return;
