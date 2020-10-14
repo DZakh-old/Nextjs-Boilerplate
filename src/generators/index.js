21d56f973163646cc4709cb75572eb2d8b5523dd
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-10-15 02:03:42 +0300

    Start adding plop generators

---
+++ src/generators/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+
+/**
+ * generator/index.js
+ *
+ * Exports the generators so plop knows them
+ */
+
+const { execSync } = require('child_process');
+const path = require('path');
+// import fs from 'fs';
+
+const appRootPath = require('app-root-path').path;
+
+const { componentGenerator } = require('./component');
+
+module.exports = (plop) => {
+  plop.setGenerator('component', componentGenerator);
+  // plop.setGenerator('container', containerGenerator);
+  // plop.setGenerator('language', languageGenerator);
+  // plop.addHelper('directory', (comp) => {
+  //   try {
+  //     fs.accessSync(path.join(__dirname, `../../app/containers/${comp}`), fs.F_OK);
+  //     return `containers/${comp}`;
+  //   } catch (e) {
+  //     return `components/${comp}`;
+  //   }
+  // });
+  // plop.addHelper('curly', (object, open) => (open ? '{' : '}'));
+  plop.setActionType('prettify', (answers, config) => {
+    const folderPath = path.join(
+      appRootPath,
+      '/src/',
+      config.path,
+      plop.getHelper('properCase')(answers.name),
+      '**'
+    );
+
+    execSync(`npm run prettify -- "${folderPath}"`);
+    return folderPath;
+  });
+};
---
+++ src/generators/utils/componentExists.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+/**
+ * componentExists
+ *
+ * Check whether the given component exist in either the components or containers directory
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const appRootPath = require('app-root-path').path;
+
+const includes = require('lodash/includes');
+
+const pageComponents = fs.readdirSync(path.join(appRootPath, '/src/components'));
+// const pageContainers = fs.readdirSync(path.join(__dirname, '../../../src/containers'));
+const components = pageComponents;
+
+function componentExists(comp) {
+  return includes(components, comp);
+}
+
+module.exports = { componentExists };
