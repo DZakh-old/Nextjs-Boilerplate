eddbee1f12becd38cf72a6915a03a12911d03102
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-11-07 19:27:29 +0300

    Add hooks folder

---
+++ src/hooks/use-change-effect.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+import noop from 'lodash/noop';
+
+export const useChangeEffect = (
+  callback: React.EffectCallback,
+  dependencies: React.DependencyList
+): void => {
+  const shouldSkipEffectRef = useRef(true);
+
+  useEffect(() => {
+    if (shouldSkipEffectRef.current) {
+      shouldSkipEffectRef.current = false;
+      return noop;
+    }
+
+    return callback();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, dependencies);
+};
