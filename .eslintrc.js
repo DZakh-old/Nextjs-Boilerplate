94a06e46ed1808fa02cd001449a0e6561b34e70c
Dmitry Zakharov <dzakh.dev@gmail.com>
2021-01-17 02:17:10 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,13 @@ const cliRulesOverrides = {
 };
 
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  parserOptions: {
-    sourceType: 'module', // Allows for the use of imports
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   env: {
-    browser: true,
-    node: true,
+    es6: true,
+  },
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
   },
   settings: {
     react: {
@@ -39,7 +34,6 @@ module.exports = {
     'prettier/prettier': 'warn',
 
     'react/react-in-jsx-scope': 'off',
-    'linebreak-style': 'off',
     'react/prop-types': 'off',
     'react/jsx-filename-extension': [
       'warn',
@@ -57,10 +51,8 @@ module.exports = {
       },
     ],
 
-    // Note you must disable the base rule as it can report incorrect errors
-    // From: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
+    // Using objects with meta-data is allowed
+    'prefer-promise-reject-errors': 'off',
 
     // It's an impossible rule to follow
     'sonarjs/cognitive-complexity': 'off',
@@ -75,11 +67,22 @@ module.exports = {
     ],
     'react/jsx-one-expression-per-line': 'off',
 
+    'linebreak-style': 'off',
     'no-underscore-dangle': 'off',
     camelcase: 'off',
     'prefer-rest-params': 'off',
     curly: ['error', 'all'],
+    'arrow-body-style': ['error', 'always'],
+    'no-implicit-coercion': ['error', { allow: [] }],
+    'no-extra-boolean-cast': ['error', { enforceForLogicalOperands: true }],
     'no-console': ['error', { allow: ['error'] }],
+    'no-restricted-syntax': ['error', 'SequenceExpression'],
+
+    // Для работы с immer
+    'no-param-reassign': [
+      'error',
+      { props: true, ignorePropertyModificationsForRegex: ['[dD]raft'] },
+    ],
 
     // For the next-optimized-images
     'import/no-unresolved': [
@@ -97,15 +100,15 @@ module.exports = {
         groups: ['object', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
         pathGroups: [
           // Separate lodash methods from other libs
-          { pattern: '{lodash/*,lodash/fp/*}', group: 'external', position: 'after' },
-
-          // In some reason eslint thinks, that public is an external group
-          { pattern: 'public/**', group: 'external', position: 'after' },
+          { pattern: '{lodash,lodash/*,lodash/fp/*}', group: 'external', position: 'after' },
 
           // Split the main components of the app to avoid a solid wall of imports.
           // Other folders from the src will be imported together.
           { pattern: '@/utils/**', group: 'internal', position: 'after' },
           { pattern: '@/components/**', group: 'internal', position: 'after' },
+
+          // In some reason eslint thinks, that public is an external group
+          { pattern: 'public/**', group: 'internal', position: 'after' },
 
           { pattern: './*.module.scss', group: 'index', position: 'after' },
         ],
@@ -115,22 +118,6 @@ module.exports = {
       },
     ],
 
-    // Для работы с immer
-    'no-param-reassign': [
-      'error',
-      { props: true, ignorePropertyModificationsForRegex: ['[dD]raft', 'mstSelf'] },
-    ],
-
     ...(process.env.ESLINT_CLI ? cliRulesOverrides : {}),
   },
-  overrides: [
-    {
-      files: ['*.{ts,tsx}'],
-      extends: ['plugin:@typescript-eslint/recommended'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-  ],
 };
